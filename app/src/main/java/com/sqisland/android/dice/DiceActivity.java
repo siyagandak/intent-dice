609a620e98d3a570496feac1aaf6d7c0d0a11f0d
Kudakwashe Adwell Siyaganda <siyagandak@gmail.com>
2019-10-24 17:52:14 +0200

    DiceActivity implementation

---
+++ app/src/main/java/com/sqisland/android/dice/DiceActivity.java
@@ -0,0 +1,13 @@
+package com.sqisland.android.dice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+    }
+}
